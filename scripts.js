35bec897744f27e87f1ebf9dd9cd952ad0011525
Rexfelix <rexfelix@naver.com>
2023-05-29 00:18:43 +0900

    delete games with error

--- scripts.js
+++ scripts.js
@@ -1,25 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const games = [
     {
-      title: "Chick Ball",
-      image: "games/ChickBallWeb/icon.png",
-      description: "Chick Ball은 Egg를 쳐내는 게임입니다.\n테스트용",
-      url: "games/ChickBallWeb/index.html",
-    },
-    {
-      title: "No Poop",
-      image: "games/NoPoopWeb/icon.png",
-      description: "NoPoop is a game of a frog busy dodging poop.",
-      url: "games/NoPoopWeb/index.html",
-    },
-    {
-      title: "Speed Up",
-      image: "games/SpeedUpWeb/icon.png",
-      description:
-        "Speed Up is a car game where you avoid obstacles. The longer you survive, the faster you go",
-      url: "games/SpeedUpWeb/index.html",
-    },
-    {
       title: "CandyPang",
       image: "games/CandyPang_webGL/icon.png",
       description:
